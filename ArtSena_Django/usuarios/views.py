b4bdd9394655892febf9b58d1d5f970baee6357d
mariamcarlier00estudio-creator <mariam00estudio@gmail.com>
2026-06-26 14:30:07 -0500

    ...

---
+++ ArtSena_Django/usuarios/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def saludar(request):
+return HttpResponse("Hola Usuarios Art Sena \n, BIENVENIDOS ...")
