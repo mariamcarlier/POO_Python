4821d6e877bcec04bcb9dba325ec2f6f88daf029
mariamcarlier <chatdesarrolloadso@gmail.com>
2026-07-22 10:18:40 -0500

    registro de modelo admin.py con registro decorador de la guia

--- ArtSena_Django/usuarios/admin.py
+++ ArtSena_Django/usuarios/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import ObraArte
 
-admin.site.register(ObraArte)
+#admin.site.register(ObraArte) forma de regitro simple
+
+@admin.register(ObraArte)  # forma de registro con decorador
+class ObraArteAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'fecha_creacion', 'precio')  # Campos que se mostrarán en la lista de administración
+    search_fields = ('titulo', 'autor')  # Campos por los que se puede buscar
+    list_filter = ('fecha_creacion',)  # Filtros para la lista de administración
+    ordering = ('-fecha_creacion',)  # Ordenamiento por defecto
+    
+
 
 # Register your models here.
